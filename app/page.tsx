7851b9935b68a1e21c01fb9f376c218470dba445
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-01-05 21:03:41 -0300

    feat: consuming the api from ExchangeRate-API and fetching it from SSR on Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-export default function Home() {
+interface ExchangeRates {
+  [currency: string]: string;
+}
+interface ApiResponse {
+  rates: ExchangeRates;
+}
+
+export default async function Home() {
+  const API_KEY = process.env.EXCHANGE_RATE_API_KEY;
+  const BASE_URL = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}`;
+
+  const res = await fetch(BASE_URL, { cache: "no-store" }); // 'no-store' evita cache
+  const data: ApiResponse = await res.json();
+
+  const rates = data.rates;
+
   return (
     <div>
-      <p>HomePage</p>
-      <h1>Taxa de cambio</h1>
-      <></>
+      <h1>Taxas de Câmbio</h1>
+      <ul>
+        {Object.entries(rates).map(([currency, rate]) => (
+          <li key={currency}>
+            {currency}: {rate}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
