3824dce6db5289b79320433bd6adff3e330b7d95
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-01-06 10:35:32 -0300

    feat: add fetching api data

--- app/Components/Container-Component/Container-Component.tsx
+++ app/Components/Container-Component/Container-Component.tsx
@@ -1,9 +1,64 @@
 "use client";
+import { useEffect, useState } from "react";
+interface ExchangeRates {
+  [currency: string]: string;
+}
+interface ApiResponse {
+  rates: ExchangeRates;
+}
 
 const ContainerComponent = () => {
+  const [rates, setRates] = useState<ExchangeRates>({});
+  const [amount, setAmount] = useState<number>(1);
+  const [fromCurrency, setFromCurrency] = useState<string>("USD");
+  const [toCurrency, setToCurrency] = useState<string>("EUR");
+  const [result, setResult] = useState<number | null>(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const API_KEY = process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY;
+        const BASE_URL = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}`;
+        const res = await fetch(BASE_URL, { cache: "no-store" });
+        if (!res.ok) {
+          throw new Error("Falha ao buscar dados da API");
+        }
+        const data: ApiResponse = await res.json();
+        setRates(data.rates);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const convertCurrency = () => {
+    const fromRate = rates[fromCurrency];
+    const toRate = rates[toCurrency];
+
+    if (fromRate && toRate) {
+      const convertedValue =
+        (amount / parseFloat(fromRate)) * parseFloat(toRate); // Cálculo de conversão
+      setResult(convertedValue);
+    } else {
+      setResult(null);
+      alert("Selecione moedas válidas.");
+    }
+  };
+
+  function formatRate(rate: string): string {
+    const rateNumber = parseFloat(rate);
+    return rateNumber.toFixed(3);
+  }
+
   return (
     <div>
       <h1>Container</h1>
+      {Object.entries(rates).map(([currency, rate]) => (
+        <li key={currency}>
+          {currency}: {formatRate(rate)}
+        </li>
+      ))}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
-interface ExchangeRates {
-  [currency: string]: string;
-}
-interface ApiResponse {
-  rates: ExchangeRates;
-}
+//Components
+import ContainerComponent from "./Components/Container-Component/Container-Component";
 
-export default async function Home() {
-  const API_KEY = process.env.EXCHANGE_RATE_API_KEY;
-  const BASE_URL = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}`;
-
-  const res = await fetch(BASE_URL, { cache: "no-store" }); // 'no-store' evita cache
-  const data: ApiResponse = await res.json();
-
-  const rates = data.rates;
-
+export default function Home() {
   return (
     <div>
       <h1>Taxas de Câmbio</h1>
       <ul>
-        {Object.entries(rates).map(([currency, rate]) => (
-          <li key={currency}>
-            {currency}: {rate}
-          </li>
-        ))}
+        <ContainerComponent />
       </ul>
     </div>
   );
